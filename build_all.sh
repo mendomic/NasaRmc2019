20dbf7a8476dcf3515cae61cccffac32ae911a02
Andrew <apw6@uw.edu>
2019-03-07 14:50:50 -0800

    Added .sh file extension for build scripts. Added conditional statement to only build raspberry pi code on armv7 arch.

--- build_all.sh
+++ build_all.sh
@@ -5,12 +5,19 @@ echo ""
 
 
 ARCH=$(uname -m)
+export ARCH=$ARCH
 if [ "$ARCH" == "aarch64" ]
 then
     CMAKE_OPTIONS="-DARCH_DIR=$ARCH -DCMAKE_C_COMPILER=/usr/bin/gcc-5 -DCMAKE_CXX_COMPILER=/usr/bin/g++-5"
 else
     CMAKE_OPTIONS="-DARCH_DIR=$ARCH"
 fi
+
+if [ "$ARCH" == "armv7l" ]
+then
+    export WIRINGPI_GPIOMEM=1
+fi
+
 
 catkin_make --cmake-args $CMAKE_OPTIONS
 
