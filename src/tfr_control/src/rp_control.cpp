a58b0375d2622e37c52dcf6e7f7c38a294875c71
Andrew Watson <apw6@uw.edu>
2019-04-07 11:01:59 -0700

    Clean up raspberry pi control node with cached param for power scale, correct mapping from velocity to wheel spin direction, and serial connection setup to outside callback.

--- src/tfr_control/src/rp_control.cpp
+++ src/tfr_control/src/rp_control.cpp
@@ -19,49 +19,29 @@ namespace wiringpi
 
 const std::string UART_DEVICE_NAME = "/dev/ttyS0";
 const int UART_BAUD_RATE = 9600;
-const int right_power_scale = 70;
-const int left_power_scale = 75;
+const int COMMAND_SIZE_BYTES = 2;
+const int MOTOR_RIGHT = 0;
+const int MOTOR_LEFT  = 1;
+int fd;
+int right_power_scale = 70;
+int left_power_scale = 75;
 
-/*
-void set_motors(const uint8_t right_motor_power, const uint8_t left_motor_power)
-{
-	const int COMMAND_SIZE_BYTES = 2;
-	uint8_t motor_power[COMMAND_SIZE_BYTES] = {right_motor_power, left_motor_power};
-	//write(fd, motor_power, COMMAND_SIZE_BYTES);
-}
-*/
 
 void serialWriteCallback(const tfr_msgs::PwmCommand & command)
 {
     //ROS_INFO("In callback");
 
-    int fd = wiringpi::serialOpen(UART_DEVICE_NAME.c_str(),
-                                  UART_BAUD_RATE);
-    if (fd < 0)
-    {
-       // ROS_ERROR("wiringPi failed to open serial device " 
-        //<< UART_DEVICE_NAME << "." << std::endl);
-    }
-
-    //ROS_INFO("wiringPi opened serial device " << UART_DEVICE_NAME 
-    //<< ", baud " << UART_BAUD_RATE << "." << std::endl);
-
-    const int COMMAND_SIZE_BYTES = 2;
     uint8_t motor_power[COMMAND_SIZE_BYTES] = {0, 0};
 
-    const int MOTOR_RIGHT = 0;
-    const int MOTOR_LEFT  = 1;
     
     if(command.enabled)
     {
-        	
-
-        write(fd, motor_power, COMMAND_SIZE_BYTES);
-
+        ros::param::getCached<int>("~right_power_scale", right_power_scale);
+        ros::param::getCached<int>("~left_power_scale", left_power_scale);
         
 
         motor_power[MOTOR_RIGHT] = command.tread_right * right_power_scale;
-        motor_power[MOTOR_LEFT] = command.tread_left * left_power_scale;
+        motor_power[MOTOR_LEFT] = -command.tread_left * left_power_scale;
         
         if (command.tread_right > 0 || command.tread_left > 0)
         {
@@ -70,13 +50,6 @@ void serialWriteCallback(const tfr_msgs::PwmCommand & command)
         }
 
         write(fd, motor_power, COMMAND_SIZE_BYTES);	
-	
-        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-
-        motor_power[MOTOR_RIGHT] = 0;
-        motor_power[MOTOR_LEFT]  = 0;
-
-        write(fd, motor_power, COMMAND_SIZE_BYTES);
     }
     else
     {
@@ -90,10 +63,22 @@ int main (int argc, char** argv)
 {
     ROS_INFO("starting raspberry pi node");
     ros::init(argc, argv, "rp_control");
+    
+    //setup serial connection
     if ( wiringpi::wiringPiSetup() == -1 )
     {
         ROS_ERROR("wiringPiSetup() failed.");
-    }   
+    }
+    fd = wiringpi::serialOpen(UART_DEVICE_NAME.c_str(), UART_BAUD_RATE);
+    if (fd < 0)
+    {
+        ROS_ERROR("wiringPi failed to open serial device %s.", UART_DEVICE_NAME);
+    }
+    else
+    {
+        ROS_INFO("wiringPi opened serial device %s, baud %d.", UART_DEVICE_NAME, UART_BAUD_RATE);
+    }
+    
     ros::NodeHandle n;
     ros::Subscriber sub_obj = n.subscribe("/motor_output", 4, serialWriteCallback);
     ROS_INFO("About to spin raspberry pi node");
