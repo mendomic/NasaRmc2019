4c151013986c5256903a4594794a3a4c6261df82
Andrew Watson <apw6@uw.edu>
2019-04-07 11:00:43 -0700

    Combined drivebase node into single cpp in drivebase_publisher.cpp

--- src/tfr_control/src/drivebase_publisher.cpp
+++ src/tfr_control/src/drivebase_publisher.cpp
@@ -37,3 +37,33 @@ namespace tfr_control
     }
 
 }
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "drivebase");
+
+    ros::NodeHandle n;
+    
+    double wheel_span, wheel_radius;
+
+    ros::param::param<double>("~wheel_span", wheel_span, 1);
+    if (wheel_span <= 0)
+    {
+        ROS_ERROR("Parameter 'wheel_span' must be a positive value.");
+        return 1;
+    }
+
+    ros::param::param<double>("~wheel_radius", wheel_radius, 1);
+    if (wheel_radius <= 0)
+    {
+        ROS_ERROR("Parameter 'wheel_radius' must be a positive value.");
+        return 1;
+    }
+    
+    tfr_control::DrivebasePublisher publisher(n, wheel_span, wheel_radius);
+    
+    ROS_INFO("drivebase started");
+
+    ros::spin();
+    return 0;
+}
