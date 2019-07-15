491e70547930a1cf86a863489db995a4f150491c
apw6 <watsonap6@gmail.com>
2019-07-15 01:28:40 -0700

    update ros package dependency install script with additional deps and to automatically change name based on ubuntu version. Changed ros realsense tag to match librealsense version installed in script. Added line in travis to install librealsense

--- apt_install_dependencies.sh
+++ apt_install_dependencies.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-sudo apt-get install ros-kinetic-desktop-full ros-kinetic-controller-manager ros-kinetic-move-base ros-kinetic-hardware-interface ros-kinetic-effort-controllers ros-kinetic-joint-state-controller ros-kinetic-joint-trajectory-controller libsfml-dev ros-kinetic-gps-common ros-kinetic-move-base ros-kinetic-moveit ros-kinetic-moveit-ros ros-kinetic-cv-camera ros-kinetic-vision-opencv ros-kinetic-rosserial ros-kinetic-rosserial-arduino ros-kinetic-rqt ros-kinetic-rqt-common-plugins ros-kinetic-rqt-robot-plugins qtbase5-dev ros-kinetic-ros-control ros-kinetic-ros-controllers libi2c-dev libgtest-dev ros-kinetic-robot-localization ros-kinetic-openni-launch -y
+ros_version=""
+version=$(lsb_release -rs)
+echo $version
+
+if [ $version == "16.04" ]; then
+  ros_version="kinetic"
+elif [ $version == "18.04" ]; then
+  ros_version="melodic"
+else
+  echo "Unknown/unsupported distro"
+  exit
+fi
+sudo apt-get update;
+sudo apt-get install ros-$ros_version-desktop-full ros-$ros_version-controller-manager ros-$ros_version-move-base ros-$ros_version-hardware-interface ros-$ros_version-effort-controllers ros-$ros_version-joint-state-controller ros-$ros_version-joint-trajectory-controller libsfml-dev ros-$ros_version-gps-common ros-$ros_version-move-base ros-$ros_version-moveit ros-$ros_version-moveit-ros ros-$ros_version-cv-camera ros-$ros_version-vision-opencv ros-$ros_version-rosserial ros-$ros_version-rosserial-arduino ros-$ros_version-rqt ros-$ros_version-rqt-common-plugins ros-$ros_version-rqt-robot-plugins qtbase5-dev ros-$ros_version-ros-control ros-$ros_version-ros-controllers libi2c-dev libgtest-dev ros-$ros_version-robot-localization ros-$ros_version-openni-launch ros-$ros_version-ddynamic-reconfigure -y
