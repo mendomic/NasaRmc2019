6531f924c202989e75d7a578013f2b3d0b44913c
apw6 <watsonap6@gmail.com>
2019-12-20 20:27:43 -0800

    Revert "Change speed publisher to actually publish speed (meters/seconds) instead of distance (meters)"
    This reverts commit 5a45247fff8546ce283b7b5ae6cb0de7078059d9.

--- src/tfr_sensor/src/tread_speed_publisher.cpp
+++ src/tfr_sensor/src/tread_speed_publisher.cpp
@@ -8,22 +8,12 @@ class TreadSpeed {
 public:
     double speed;
 
-    TreadSpeed(const int ticksPerRevolution, const int maxTicks, const double wheelRadius, const int prevTickCount = 0, const ros::Time * prevTime= nullptr):
-        speed{ 0 }, prevTickCount{ prevTickCount }, ticksPerRevolution{ ticksPerRevolution }, maxTicks{ maxTicks }, wheelRadius{ wheelRadius } {
-            if (prevTime == nullptr){
-                this->prevTime = ros::Time::now();
-            } else {
-                this->prevTime = *prevTime;
-            }
-        }
+    TreadSpeed(const int ticksPerRevolution, const int maxTicks, const double wheelRadius, const int prevTickCount = 0) :
+        speed{ 0 }, prevTickCount{ prevTickCount }, ticksPerRevolution{ ticksPerRevolution }, maxTicks{ maxTicks }, wheelRadius{ wheelRadius } {}
 
     void updateFromNewCount(const int newCount) {
-        auto currentTime = ros::Time::now();
         auto ticksMoved = calcTickDiff(newCount);
-        auto distanceTraveled = (wheelRadius * ticksMoved) / ticksPerRevolution;
-        double elapsedSeconds = (currentTime - prevTime).toSec();
-        speed = distanceTraveled/elapsedSeconds;
-        prevTime = currentTime;
+        speed = (wheelRadius * ticksMoved) / ticksPerRevolution;
         prevTickCount = newCount;
     }
     
@@ -37,7 +27,6 @@ private:
     }
 
     int prevTickCount; // last recorded position of wheel
-    ros::Time prevTime;
     const int ticksPerRevolution; // number of ticks counted each revolution of the measured wheel
     const int maxTicks; // number of ticks counted before rolling over back to 0
     const double wheelRadius; // radius of wheel (for which ticks are being counted) in meters
